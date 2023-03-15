7bed4983190642887597564b3420ca603e15a2c4
minsoo <minsu2344@naver.com>
2023-03-15 22:54:05 +0900

    [PGS] 땅따먹기 / Lv.2

---
+++ iYoiYo/3월 3주차/[PGS]땅따먹기.js
@@ -0,0 +1,11 @@
+function solution(land) {
+  let answer = 0;
+
+  for(let i=1; i<land.length; i++) {
+      for(let j=0; j<4; j++) {
+          land[i][j] += Math.max(land[i-1][(j+1)%4], land[i-1][(j+2)%4], land[i-1][(j+3)%4]);
+      }
+  }
+  
+  return Math.max(...land[land.length-1]);
+}
