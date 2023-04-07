2a78ec883893eee0bf4dc5f9d7a089752ba25b69
minsoo <minsu2344@naver.com>
2023-04-08 00:47:34 +0900

    [PGS] 삼각 달팽이  / Lv.2

--- iYoiYo/4월 1주차/[PGS]삼각_달팽이.js
+++ iYoiYo/4월 1주차/[PGS]삼각_달팽이.js
@@ -1,53 +1,52 @@
 // 엘리스 코테에 나왔던 문제랑 비슷해서 살았다^_^
-// function solution(n) {
-//     const ans = [];
-//     let max = 0, c = 1, dir = 'd', x = 0, y =0;
-    
-//     for(let i=1; i<=n; i++) {
-//         const line = Array(i).fill(0);
-//         ans.push(line);
-//         max += i;
-//     }
-    
-//     while(c <= max) {
-//         ans[x][y] = c
-        
-//         switch(dir) {
-//             case 'd':
-//                 if(x+1<n && ans[x+1][y] === 0) x++
-//                 else {
-//                     dir = 'r';
-//                     y++;
-//                 }
-//                 break;
-//             case 'r':
-//                 if(ans[x][y+1] === 0) y++
-//                 else {
-//                     dir = 'u';
-//                     x--;
-//                     y--;
-//                 }
-//                 break;
-//             case 'u':
-//                 if(ans[x-1][y-1] === 0) {
-//                     x--;
-//                     y--;
-//                 }
-//                 else {
-//                     dir = 'd';
-//                     x++;
-//                 }
-//                 break;
-//         }
-//         c++
-//     }
-    
-//     const result = [];
-    
-//     ans.forEach(arr => result.push(...arr));
-    
-//     return result;
-// }
+function solution(n) {
+  const ans = [];
+  let c = 1, dir = 'd', x = 0, y =0;
+  
+  for(let i=1; i<=n; i++) {
+      const line = Array(i).fill(0);
+      ans.push(line);
+  }
+  
+  while(ans[x][y] === 0) {
+      ans[x][y] = c
+      
+      switch(dir) {
+          case 'd':
+              if(x+1<n && ans[x+1][y] === 0) x++
+              else {
+                  dir = 'r';
+                  y++;
+              }
+              break;
+          case 'r':
+              if(ans[x][y+1] === 0) y++
+              else {
+                  dir = 'u';
+                  x--;
+                  y--;
+              }
+              break;
+          case 'u':
+              if(ans[x-1][y-1] === 0) {
+                  x--;
+                  y--;
+              }
+              else {
+                  dir = 'd';
+                  x++;
+              }
+              break;
+      }
+      c++
+  }
+  
+  const result = [];
+  
+  ans.forEach(arr => result.push(...arr));
+  
+  return result;
+}
 
 // 이런 방법도 있지만 시간 비슷하니까 내 풀이가 암튼 정석임
 // array.flat()이 시간 오래걸림
