1eb39d6ef0c753f1a08026638c7f3ee02ccf12b3
minsoo <minsu2344@naver.com>
2023-06-03 11:46:58 +0900

    마법의 엘리베이터 / Lv.2

---
+++ iYoiYo/6월 1주차/마법의_엘리베이터.js
@@ -0,0 +1,35 @@
+// bfs
+function solution(storey) {
+  const ans = [];
+  
+  const newStorey = storey.toString().split('').map(Number);
+  
+  function dfs(i, sum, arr) {
+      if(arr[i] === 10) {
+          const newArr = [...arr];
+          if(i === 0) {
+              newArr.splice(i, 1, 1, 0);
+              i++;
+          }
+          else newArr[i-1] += 1;
+          
+          dfs(i-1, sum, newArr);
+          return;
+      }
+      
+      if(i !== 0) {
+          dfs(i-1, sum+arr[i], arr);
+          const newArr = [...arr];
+          newArr[i-1] += 1;
+          dfs(i-1, sum+(10-arr[i]), newArr);
+      }
+      else {
+          ans.push(sum+arr[0]);
+          ans.push(sum+(11-arr[0]))
+      }
+  }
+  
+  dfs(newStorey.length-1, 0, newStorey)
+  
+  return Math.min(...ans)
+}
