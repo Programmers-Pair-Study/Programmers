449822f1ed9e23bb1899b6b91d0fa646df0eba9b
minsoo <minsu2344@naver.com>
2023-06-06 20:59:29 +0900

    숫자 카드 나누기 / Lv.2

--- iYoiYo/6월 2주차/숫자_카드_나누기.js
+++ iYoiYo/6월 2주차/숫자_카드_나누기.js
@@ -48,19 +48,6 @@ function solution(arrayA, arrayB) {
 
 // some을 이용한 풀이
 function solution(arrayA, arrayB) {
-arrayA.sort((a, b) => a - b);
-arrayB.sort((a, b) => a - b);
-// curr은 모두 나누고 target은 모두 나눌 수 없는 최대값을 도출하는 함수 입니다
-const findA = (curr, target) => {
-  for (let i = curr[0]; i > 0; i--) {
-    if (curr.every((v) => v % i === 0) && !target.some((v) => v % i === 0)) return i;
-  }
-  return 0;
-};
-return Math.max(findA(arrayA, arrayB), findA(arrayB, arrayA));
-}
-
-function solution(arrayA, arrayB) {
   // 1) 철수만 모든 숫자를 나눌수 있는 경우 
   let a = getDvsr(arrayA, arrayB);
 
