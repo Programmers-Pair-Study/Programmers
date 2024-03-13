fd7f94473c4ca0ac9a4d2d5626dfa839286dedbe
sandeulju <tksemf7410@gmail.com>
2024-03-13 23:38:28 +0900

    [BOJ] 새싹 / 새싹 난이도

---
+++ SanDeul/2024/3월 3주차/[BOJ]새싹.js
@@ -0,0 +1,8 @@
+console.log(`         ,r\'\"7`);
+console.log(`r\`-_   ,\'  ,/`);
+console.log(` \\. \". L_r\'`);
+console.log(`   \`~\\/`);
+console.log(`      |`);
+console.log(`      |`);
+
+// 이거 풀어야 은메달이라 ㅎㅎ ㅈㅅ;;
